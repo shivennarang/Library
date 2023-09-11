8c8831224f2302405afce62ee9e2cfd55ebafda9
unknown <krishofficialinbox@gmail.com>
2023-09-12 00:30:22 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import BookSearch from "./pages/BookSearch";
+import BookShelf from "./pages/BookShelf";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" exact element={<BookSearch />} />
+        <Route path="/bookshelf" element={<BookShelf />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
